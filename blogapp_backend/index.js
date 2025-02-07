40f8f96c50e693e0d302c86ce0310de8b2557140
him16112 <99254194+him16112@users.noreply.github.com>
2025-02-07 19:58:06 +0530

    Update index.js

--- blogapp_backend/index.js
+++ blogapp_backend/index.js
@@ -32,7 +32,7 @@ const verifyToken = (req, res, next) => {
 };
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"

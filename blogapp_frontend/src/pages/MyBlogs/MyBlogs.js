ea0ee48deff316b49aea6543973c890d05aa5dd5
Himanshu Sharma <himanshu.sharma16112@gmail.com>
2025-02-13 15:16:32 +0530

    first commit

--- blogapp_frontend/src/pages/MyBlogs/MyBlogs.js
+++ blogapp_frontend/src/pages/MyBlogs/MyBlogs.js
@@ -1,45 +1,49 @@
-import React, {useEffect, useState } from "react";
-import { getMyBlogs} from "./MyBlogsApi";
+import React, { useEffect, useState } from "react";
+import { getMyBlogs } from "./MyBlogsApi";
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from 'react-router';
+import { useNavigate } from "react-router";
 import Navbar from "../../component/Navbar";
-
 
 const MyBlogs = () => {
   const [myBlogs, setMyBlogs] = useState([]);
-  const isBlogCreated = useSelector(state => state.Blog.isBlogCreated) 
+  const isBlogCreated = useSelector((state) => state.Blog.isBlogCreated);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const loading = useSelector((state) => state.Blog.loading);
 
   useEffect(() => {
-    const funCall = async() =>{
-      await getMyBlogs(setMyBlogs, dispatch); 
-    }
+    const funCall = async () => {
+      await getMyBlogs(setMyBlogs, dispatch);
+    };
     funCall();
   }, [isBlogCreated, dispatch]);
 
   const blogView = (blogId) => {
     navigate("/blog", { state: { blogId } });
-  }
+  };
 
   return (
     <>
-     <Navbar/>
-      <div className="home-container">
-        <ul className="blog-list">
-          {myBlogs?.map((blog) => (
-            <li
-              key={blog._id}
-              className="blog-item"
-              onClick={() => blogView(blog._id)}
-            >
-              <div className="title">{blog.title}</div>
-              <div className="paragraph">{blog.paragraph}</div>
-              <div className="username">{blog.username}</div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Navbar />
+      {loading ? (
+        <p>My Blogs Loading...</p>
+      ) : (
+        <div className="home-container">
+          <ul className="blog-list">
+            {myBlogs?.map((blog) => (
+              <li
+                key={blog._id}
+                className="blog-item"
+                onClick={() => blogView(blog._id)}
+              >
+                <div className="title">{blog.title}</div>
+                <div className="paragraph">{blog.paragraph}</div>
+                <div className="username">{blog.username}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
 };
